11fc9c6fded2f172c3c68f7738c9bf341a965e27
aly50n <alyson.morato13@gmail.com>
2024-09-30 20:45:00 -0300

    feat: inputs e exibição dos dados

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,25 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        int numero;
+        String agencia;
+        String nomeCliente;
+        double saldo;
+        
+
+        System.out.println("Por favor, digite o numero da agência: ");
+        numero = scanner.nextInt();
+        scanner.nextLine(); // Consumindo a linha que ficou no buffer
+        System.out.println("Por favor, digite o nome da agência: ");
+        agencia = scanner.nextLine();
+        System.out.println("Por favor, digite o nome do cliente: ");
+        nomeCliente = scanner.nextLine();
+        System.out.println("Por favor, digite o seu saldo: ");
+        saldo = scanner.nextDouble();
+        scanner.nextLine(); // Consumindo a linha que ficou no buffer
+        System.out.println("Olá "+nomeCliente+", obrigado por criar uma conta em nosso banco, sua agência é "+agencia+", conta "+ numero +" e seu saldo R$ "+ saldo +" já está disponível para saque!");
+        
+
     }
 }
