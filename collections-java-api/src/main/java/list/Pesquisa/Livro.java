d0e3b29c653eef424a38622d7149123f08f28b3c
aly50n <alyson.morato13@gmail.com>
2024-10-12 09:57:26 -0300

    feat: Classe e metodos de Livro

---
+++ collections-java-api/src/main/java/list/Pesquisa/Livro.java
@@ -0,0 +1,42 @@
+package main.java.list.Pesquisa;
+
+public class Livro {
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+    
+    public Livro(String titulo, String autor, int anoPublicacao){
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    @Override
+    public String toString() {
+        return "Livro titulo: "+ getTitulo()+", Autor: "+getAutor()+", Ano de publicação: "+getAnoPublicacao()+".";
+    }
+}
