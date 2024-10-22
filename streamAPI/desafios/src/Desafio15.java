20a8a59c2471488d7929731ea4371be002ecbfef
aly50n <alyson.morato13@gmail.com>
2024-10-22 17:11:40 -0300

    Resolução do DESAFIO15 de streamAPI.

---
+++ streamAPI/desafios/src/Desafio15.java
@@ -0,0 +1,21 @@
+/*Desafio 15 - Verifique se a lista contém pelo menos um número negativo:
+* Utilizando a Stream API, verifique se a lista contém pelo menos um número 
+* negativo e exiba o resultado no console.
+*/
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Desafio15 {
+    public static void main(String[] args) throws Exception {
+
+        List<Integer> numeros = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 5, 4, 3);
+
+        if (!numeros.stream().allMatch(n -> n > 0)) {
+            System.out.println("A lista possui pelo menos um número negativo.");
+        } else {
+            System.out.println("A lista não possui numeros negativos!");
+        }
+
+    }
+}
