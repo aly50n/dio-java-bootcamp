6045f70d6cc1f404a4aabe362a30f269f017b4be
aly50n <alyson.morato13@gmail.com>
2024-10-26 15:02:39 -0300

    Feat: metodos de adicionar, remover, pesquisar por numero da conta e cpf do cliente e listar contas no banco.

--- banco-digital/src/models/Banco.java
+++ banco-digital/src/models/Banco.java
@@ -1,7 +1,11 @@
 package models;
+
 import java.util.ArrayList;
 import java.util.List;
 
+import lombok.Data;
+
+@Data
 public class Banco {
 
     private String nome;
@@ -12,14 +16,41 @@ public class Banco {
         this.contas = new ArrayList<>();
     }
 
-    public String getNome() {
-        return nome;
+    public void adicionarConta(Conta conta) {
+        contas.add(conta);
     }
 
-    public List<Conta> getContas() {
-        return contas;
-    }
+    public Conta pesquisarContaClientePorCpf(String Cpf) {
 
+        return contas.stream()
+                .filter(x -> Cpf.equalsIgnoreCase(x.getCliente().getCpf()))
+                .findFirst()
+                .orElse(null);
     
+    }
+
+    public Conta pesquisarContaPorNumeroConta(int numeroConta) {
+
+        return contas.stream()
+                .filter(x -> numeroConta == x.getNumeroConta())
+                .findFirst()
+                .orElse(null);
+    
+    }
+
+    public void removerConta(int numeroConta){
+        Conta contaParaRemover = contas.stream().filter(x -> numeroConta == x.getNumeroConta()).findFirst().orElse(null);
+        if(contaParaRemover != null){
+            System.out.println("Conta removida: "+ contaParaRemover);
+            contas.remove(contaParaRemover);
+        }
+        else{
+            System.out.println("Conta não encontrada!");
+        }
+    }
+
+    public void listarContas(){
+        contas.stream().forEach(System.out::println);
+    }
 
 }
