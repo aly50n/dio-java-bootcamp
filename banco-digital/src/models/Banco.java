933d902462ab7bb87f4504219a6bbf659d8b1020
aly50n <alyson.morato13@gmail.com>
2024-10-25 21:53:24 -0300

    Feat: Classe banco

---
+++ banco-digital/src/models/Banco.java
@@ -0,0 +1,25 @@
+package models;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Banco {
+
+    private String nome;
+    private List<Conta> contas;
+
+    public Banco(String nome) {
+        this.nome = nome;
+        this.contas = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public List<Conta> getContas() {
+        return contas;
+    }
+
+    
+
+}
