e9b60275d4626047e11aa27c30b7648201c0d8af
aly50n <alyson.morato13@gmail.com>
2024-10-26 15:01:50 -0300

    Atualização do main testando a classe banco

--- banco-digital/src/Main.java
+++ banco-digital/src/Main.java
@@ -1,3 +1,4 @@
+import models.Banco;
 import models.Cliente;
 import models.Conta;
 import models.ContaCorrente;
@@ -7,13 +8,22 @@ public class Main {
     public static void main(String[] args) throws Exception {
         Cliente alyson = new Cliente();
         alyson.setNome("Alyson");
+        alyson.setCpf("123.456.778-00");
+
+        Cliente roberto = new Cliente();
+        roberto.setNome("roberto");
+        roberto.setCpf("213.555.687-88");
+
         Conta contaC = new ContaCorrente(alyson);
-        Conta contaP = new ContaPoupanca(alyson);
-        contaC.depositar(100.0);
-        contaC.imprimirExtrato();
-        contaP.imprimirExtrato();
-        contaC.transferirPix(100d,contaP);
-        contaP.imprimirExtrato();
+        Conta contaP = new ContaPoupanca(roberto);
+        contaC.depositar(100d);
+        contaC.transferirPix(100d, contaP);
+
+        Banco banco = new Banco("Roxinho");
+        banco.adicionarConta(contaP);
+        banco.adicionarConta(contaC);
+        banco.removerConta(1);
+        banco.listarContas();
 
     }
 }
