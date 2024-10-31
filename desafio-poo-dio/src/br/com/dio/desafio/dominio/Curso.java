43b2b0a2239749fcce905e9fa3f6757ead543110
aly50n <alyson.morato13@gmail.com>
2024-10-31 19:03:08 -0300

    chore: Adicionado @AllArgsConstructor e @NoArgsConstructor na classe.

--- desafio-poo-dio/src/br/com/dio/desafio/dominio/Curso.java
+++ desafio-poo-dio/src/br/com/dio/desafio/dominio/Curso.java
@@ -1,12 +1,16 @@
 package br.com.dio.desafio.dominio;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Curso extends Conteudo {
     private String titulo;
     private String descricao;
