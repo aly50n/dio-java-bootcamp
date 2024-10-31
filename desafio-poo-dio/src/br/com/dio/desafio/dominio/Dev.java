a8cace29a0f2029911c554053d29490faa8159e1
aly50n <alyson.morato13@gmail.com>
2024-10-31 19:03:52 -0300

    chore: Adicionado @AllArgsConstructor e @NoArgsConstructor na classe.

--- desafio-poo-dio/src/br/com/dio/desafio/dominio/Dev.java
+++ desafio-poo-dio/src/br/com/dio/desafio/dominio/Dev.java
@@ -4,9 +4,13 @@ import java.util.LinkedHashSet;
 import java.util.Optional;
 import java.util.Set;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class Dev {
     private String nome;
     private Set<Conteudo> conteudosInscritos = new LinkedHashSet<>();
