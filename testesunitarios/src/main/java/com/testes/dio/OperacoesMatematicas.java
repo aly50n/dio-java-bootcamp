244e7d4475fe2ae65ca3d6a0da62e8c4c78cba71
aly50n <alyson.morato13@gmail.com>
2024-11-03 19:52:22 -0300

    Feat: Classe com metodos de Operações matemáticas

---
+++ testesunitarios/src/main/java/com/testes/dio/OperacoesMatematicas.java
@@ -0,0 +1,21 @@
+package com.testes.dio;
+
+public class OperacoesMatematicas {
+    
+    public int somar(int a, int b){
+        return a+b;
+    }
+
+    public int subtracao(int a, int b){
+        return a-b;
+    }
+
+    public double divisao(int a, int b){
+        return a/b;
+    }
+
+    public double multiplicacao(int a, int b){
+        return a*b;
+    }
+
+}
