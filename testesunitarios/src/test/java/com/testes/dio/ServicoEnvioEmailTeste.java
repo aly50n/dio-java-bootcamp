82254db6ba5b01d2d33fb0ca3caa3d3a2771ed91
aly50n <alyson.morato13@gmail.com>
2024-11-04 20:12:10 -0300

    Test: Testes unitários para ServicoEnvioEmail

---
+++ testesunitarios/src/test/java/com/testes/dio/ServicoEnvioEmailTeste.java
@@ -0,0 +1,44 @@
+package com.testes.dio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ServicoEnvioEmailTeste {
+
+    @Mock
+    private PlataformaDeEnvio plataforma;
+
+    @InjectMocks
+    private ServicoEnvioEmail servico;
+
+    @Captor
+    private ArgumentCaptor<Email> captor;
+
+    @Test
+    void validarDadosEnviadosParaAPlataforma() {
+
+        String enderecoDeEmail = "usuarioTeste@teste.com";
+        String mensagem = "Olá mundo teste";
+        boolean ehFormatoHtml = false;
+
+        servico.enviaEmail(enderecoDeEmail, mensagem, ehFormatoHtml);
+
+        Mockito.verify(plataforma).enviaEmail(captor.capture());
+
+        Email emailCapturado = captor.getValue();
+
+        Assertions.assertEquals(mensagem, emailCapturado.getMensagem());
+        Assertions.assertEquals(enderecoDeEmail, emailCapturado.getEnderecoEmail());
+        Assertions.assertEquals(Formato.TEXTO, emailCapturado.getFormato());
+
+    }
+
+}
