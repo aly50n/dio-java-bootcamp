57cc45ab71abd681fe6b72de6292074017978741
aly50n <alyson.morato13@gmail.com>
2024-11-15 09:51:21 -0300

    reorganização do package

---
+++ lab-padroes-de-projeto-java/src/dio/bootcamp/gof/Singleton/SingletonEager.java
@@ -0,0 +1,20 @@
+package dio.bootcamp.gof.Singleton;
+
+/* Singleton "apressado" */
+
+public class SingletonEager {
+   
+    private static SingletonEager instancia = new SingletonEager();
+
+    private SingletonEager(){
+        super();
+    }
+
+    public static SingletonEager getInstancia(){
+        return instancia;
+    }
+
+
+
+
+}
